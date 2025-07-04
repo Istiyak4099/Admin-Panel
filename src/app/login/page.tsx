4769d47d29b1dee16f61b2149331de0cec23bb9d
Abdul Hadi Istiyak <abdulhadiistiyak@gmail.com>
2025-07-04 12:54:33 +0000

    after you made that change i re deployed but it is still showing me the

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 import { LoginHub } from '@/components/login-hub';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
