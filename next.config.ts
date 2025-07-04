3eda2c8da1fc7669e6c77e8a1b2fca4f874c8227
Abdul Hadi Istiyak <abdulhadiistiyak@gmail.com>
2025-07-04 03:40:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
