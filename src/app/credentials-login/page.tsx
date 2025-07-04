de418cb6f4aa905c3a026bf6b4ff3857aade3f78
Abdul Hadi Istiyak <abdulhadiistiyak@gmail.com>
2025-07-04 12:14:33 +0000

    why is my publishing failing?

--- src/app/credentials-login/page.tsx
+++ src/app/credentials-login/page.tsx
@@ -1,5 +1,7 @@
 import { CredentialsLoginForm } from "@/components/credentials-login-form";
 
+export const dynamic = 'force-dynamic';
+
 export default function CredentialsLoginPage() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
