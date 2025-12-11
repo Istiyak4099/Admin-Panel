c44ccffc7e379ed4663dc62977896174f046e35a
Istiyak4099 <abdulhadiistiyak@gmail.com>
2025-12-11 14:16:27 +0600

    Update page.tsx

--- src/app/credentials-login/page.tsx
+++ src/app/credentials-login/page.tsx
@@ -1,5 +1,16 @@
 
 import { CredentialsLoginForm } from "@/components/credentials-login-form";
+import React, { Suspense } from "react";
+import { LoaderCircle } from "lucide-react";
+
+function LoginPageLoading() {
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <LoaderCircle className="h-8 w-8 animate-spin" />
+            <p className="text-muted-foreground">Loading Login Form...</p>
+        </div>
+    )
+}
 
 export default function CredentialsLoginPage() {
   return (
@@ -8,7 +19,10 @@ export default function CredentialsLoginPage() {
          <h1 className="text-4xl font-bold tracking-tight">EMI Locker System</h1>
          <p className="text-muted-foreground">Enter your credentials to sign in</p>
       </div>
-      <CredentialsLoginForm />
+      <Suspense fallback={<LoginPageLoading />}>
+        <CredentialsLoginForm />
+      </Suspense>
     </div>
   );
 }
+
